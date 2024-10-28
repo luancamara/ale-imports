3ddb840dfbcda4f9fc5d4aecbefc019d2f7331d3
Luan Camara <luancamara@gmail.com>
2024-10-28 14:28:12 -0300

    lint fix

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import * as React from "react"
 import { Cross2Icon } from "@radix-ui/react-icons"
 import * as ToastPrimitives from "@radix-ui/react-toast"
 import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -83,7 +83,7 @@ const ToastClose = React.forwardRef<
     toast-close=""
     {...props}
   >
-    <Cross2Icon className="h-4 w-4" />
+    <Cross2Icon className="size-4" />
   </ToastPrimitives.Close>
 ))
 ToastClose.displayName = ToastPrimitives.Close.displayName
